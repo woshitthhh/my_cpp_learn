a0706894dd64eb932b53c4971a23eb6b68d8ca80
nwumath <2940878756@qq.com>
2023-11-27 12:15:42 +0800

    virtual

--- C++fundamentals/class/VirtualFunction/LearnVirtual.cpp
+++ C++fundamentals/class/VirtualFunction/LearnVirtual.cpp
@@ -32,11 +32,11 @@ void PrintName(Entity *entity)
 
 int main()
 {
-    // Entity e;
+    // Entity e; //虚函数必须由基类指针进行访问，对象名加点运算符访问虚函数时静态调用，不会用到虚函数的特性
     // cout << e.GetName() << endl;
     // PrintName(e);
     
-    Player p("zhou");
-    Entity *e = &p;
+    Player p("zhou"); 
+    Entity *e = &p; //定义指向Player对象的基类指针，这样才能展现出虚函数的特点
     cout << e->GetName() << endl;
 }
